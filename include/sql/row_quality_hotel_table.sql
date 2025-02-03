43d9e97e65d5e1c4951d4735c6e09c581a85ac62
Matthew Cooper <mscoop@umich.edu>
2025-02-03 17:11:50 -0500

    Adding quality checks for hotel data

---
+++ include/sql/row_quality_hotel_table.sql
@@ -0,0 +1,5 @@
+SELECT 
+  CASE WHEN RUN_DATE < CHECK_IN_DATE THEN 1 ELSE 0 END AS run_date_check,
+  CASE WHEN PRICE > 0 THEN 1 ELSE 0 END AS price_check
+FROM {{ params.table }}
+WHERE RUN_DATE IN (SELECT RUN_DATE FROM {{ params.table }} ORDER BY RANDOM() LIMIT 1)
